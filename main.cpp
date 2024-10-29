d344460138b736426e92e28f487f3e2234253dfd
Allie1080 <esparesamrei@gmail.com>
2024-10-30 01:27:31 +0800

    compilation error with stack pop() and .push()
    No idea what's wrong with them.
    
    .pop() error: request for member '~int [2]' in '* __p', which is of non-class type 'int [2]' (Something to do with the deconstructor?)
    
    .push() error: parenthesized initializer in array new

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
 #include <stack>
 #include <array>
 
+const int MAX_SIZE = 5; 
 const std::string pillarOutOfRange = "That pillar is out of range!";
 const std::string tooManyArguments = "Too many arguments!";
 const std::string notInteger = "The provided arguments are not integers!";
+const std::string pillarFull = "That pillar is already fool!";
+// error messages
 
 void displayError (std::string error) {
     std::cout << "[ERROR]" << '\n';
@@ -25,7 +28,7 @@ bool isdigit (std::string text) {
      return true;
 }
 
-int convertStringToInt(std::string text) {
+int convertStringToInt (std::string text) {
     int number{0};
 
     if (!isdigit(text)) {
@@ -40,7 +43,81 @@ int convertStringToInt(std::string text) {
     return number;
 }
 
-void parseInput (std::string input, std::stack<int> pillars[], std::stack<int[2]> &backHistory, std::stack<int[2]> &frontHistory) {
+void swapNumbers (int &num1, int &num2) {
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+void makeRingsFall (std::stack<int> *pillars[]) {
+    // for rendering purposes
+    // just pass the array by pointer next time
+    // if you're gonna pass-by-reference, declare the argument as a *const 
+    // actually, it still wouldn't work for this one even if you declared it a *const
+    // wait no, it does work but only if you remove []
+    // man, what the fuck
+
+
+    for (int pillarIndex{0}; pillarIndex < 3; pillarIndex++) {
+        int amountOfZeroesNeeded = 5 - pillars[pillarIndex]->size();
+
+        if (amountOfZeroesNeeded == 0) {
+            continue;
+        }
+
+        for (int layer{0}; layer < amountOfZeroesNeeded; layer++) {
+            pillars[pillarIndex]->push(0);
+        }
+    }
+}
+
+void displayPillars (std::stack<int> pillars[]) {
+    makeRingsFall(&pillars);
+
+    for (int layer{0}; layer < 5; layer++) {
+        std::cout << pillars[0].top() << " " << pillars[1].top() << " " << pillars[2].top() << '\n';
+        pillars[0].pop();
+        pillars[1].pop();
+        pillars[2].pop();
+    }
+}
+
+void moveRing (std::stack<int> (&pillars)[], int oldPillar, int targetPillar, std::stack<int[2]> &history, bool isUndo=false) {
+    // if isUndo, use frontHistory for history parameter
+    if (oldPillar < 0 | oldPillar > 2 | targetPillar < 0 | targetPillar > 2) {
+        displayError(pillarOutOfRange);
+        return;
+
+    }
+
+    if (pillars[targetPillar].size() == MAX_SIZE) {
+        displayError(pillarFull);
+        return;
+
+    }
+
+    if (isUndo) {
+        swapNumbers(oldPillar, targetPillar);
+    
+    }
+
+    int event[2] = {oldPillar, targetPillar};
+
+    pillars[targetPillar].push(pillars[oldPillar].top());
+    pillars[oldPillar].pop();
+
+    history.push(event);
+
+    displayPillars(pillars);
+
+}
+
+void solvePuzzle() {
+    std::cout << "Puzzle solved!" << '\n';
+    std::cout <<'\n';
+}
+
+void parseInput (std::string input, std::stack<int> (&pillars)[], std::stack<int[2]> &backHistory, std::stack<int[2]> &frontHistory) {
     std::stringstream inputStream(input);
     std::string variable;
     std::string inputArray[64];
@@ -68,50 +145,35 @@ void parseInput (std::string input, std::stack<int> pillars[], std::stack<int[2]
 
         moveRing(pillars, oldPillar, targetPillar, backHistory);
     
-    } else if (inputArray[0] == "-r" | inputArray[0] == "--redo") {
+    } else if (inputArray[0] == "-u" | inputArray[0] == "--undo") {
+        if (inputArray[1].size() == 0) {
+            inputArray[1] = "1";
+        }
+        
+        if (!isdigit(inputArray[1])) {
+            displayError(notInteger);
+            return;
+        }
+
+        int oldPillar = backHistory.top()[0];
+        int targetPillar = backHistory.top()[1];
+
+        moveRing(pillars, oldPillar, targetPillar, frontHistory, true);
+        backHistory.pop();    
         
     } 
 }
 
-void displayPillars (std::stack<int> pillars[]) {
-    for (int layer{0}; layer < 5; layer++) {
-        std::cout << pillars[0].top() << " " << pillars[1].top() << " " << pillars[2].top() << '\n';
-        pillars[0].pop();
-        pillars[1].pop();
-        pillars[2].pop();
-    }
-}
-
-void moveRing (std::stack<int> pillars[], int &oldPillar, int &targetPillar, std::stack<int[2]> &history, bool isUndo=false) {
-    // if isUndo, use frontHistory for history parameter
-    if (oldPillar < 0 | oldPillar > 2 | targetPillar < 0 | targetPillar > 2) {
-        displayError(pillarOutOfRange);
-    }
-
-    if (isUndo) {
-
-    } else {
-    
-    }
-
-    history.push({oldPillar, targetPillar});
-
-}
-
-void solvePuzzle() {
-
-}
-
 int main () {
     std::stack<int> pillars[3];
-    std::stack<int[2]> backHistory;
-    std::stack<int[2]> frontHistory;
+    std::stack<std::array<int, 2>> backHistory;
+    std::stack<std::array<int, 2>> frontHistory;
 
-    for (int index{0}; index < 3; index++) {
-        for (int counter{0}; counter < 5; counter++) {
-            pillars[index].push(counter);
-        }
-    }
+    pillars[0].push(5);
+    pillars[0].push(4);
+    pillars[0].push(3);
+    pillars[0].push(2);
+    pillars[0].push(1);
 
     displayPillars(pillars);
 
